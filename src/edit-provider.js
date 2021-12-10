61d8663d6dba14d3c400703323db03b5a567e4a0
Dallin Osmun <dosmun@gmail.com>
2021-12-09 17:01:58 -0700

    Revert "Support forked prettier standard (#51)"
    This reverts commit b7523d8c72e217942f3659f19ccaf6dcd395e565.

--- src/edit-provider.js
+++ src/edit-provider.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const prettierStandard = require('prettier-standard')
 const { workspace, window, Range, TextEdit } = require('vscode')
 
 const { getPrettierParser } = require('./language-map')
@@ -7,26 +7,18 @@ function fullDocumentRange (document) {
   const lastLineId = document.lineCount - 1
   return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length)
 }
-const MODULES = ['@ksmithut/prettier-standard', 'prettier-standard']
 
 module.exports = class PrettierEditProvider {
   constructor () {
-    this.prettier = import('@ksmithut/prettier-standard')
-    this.loadPackagedVersion().catch(error => {
-      console.error('Error loading packaged prettier-standard', error)
-    })
+    this.prettier = prettierStandard
+    this.loadPackagedVersion()
   }
 
   async loadPackagedVersion () {
-    const PATH = `node_modules/{${MODULES.join(',')}}/package.json`
+    const PATH = 'node_modules/prettier-standard/package.json'
     const [uri] = await workspace.findFiles(PATH)
     if (!uri) return
-    const packageJSON = JSON.parse(
-      await fs.promises.readFile(uri.fsPath, 'utf-8')
-    )
-    const rootPath = uri.fsPath.replace(/\/package\.json$/, '')
-    const mainPath = `${rootPath}/${packageJSON.main}`
-    const prettier = await import(mainPath)
+    const prettier = require(uri.fsPath.replace(/\/package\.json$/, ''))
     if (prettier && prettier.resolveConfig && prettier.resolveConfig.sync) {
       this.prettier = prettier
     }
@@ -38,21 +30,19 @@ module.exports = class PrettierEditProvider {
     if (uri && uri.scheme === 'file') return uri.fsPath
   }
 
-  async getConfig (document) {
-    const prettier = await this.prettier
+  getConfig (document) {
     const opts = { editorconfig: true, useCache: false }
     const path = this.getConfigPath(document)
-    const config = (path && prettier.resolveConfig.sync(path, opts)) || {}
+    const config = (path && this.prettier.resolveConfig.sync(path, opts)) || {}
     config.filepath = '(stdin)'
     config.parser = getPrettierParser(document.languageId)
     return config
   }
 
-  async format (document, range) {
+  format (document, range) {
     try {
-      const prettier = await this.prettier
       const text = range ? document.getText(range) : document.getText()
-      const newText = prettier.format(text, await this.getConfig(document))
+      const newText = this.prettier.format(text, this.getConfig(document))
       return [TextEdit.replace(range || fullDocumentRange(document), newText)]
     } catch (e) {
       console.error(e)
